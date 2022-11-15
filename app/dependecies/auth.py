f0daa193ec3d7c8d8327c1ce6a51c9ea4fca41fb
Nifacy <mai.grishin.alexey@gmail.com>
2022-11-16 01:05:27 +0300

    Removed unused directories

--- app/dependecies/auth.py
+++
@@ -1,5 +0,0 @@
-from ..schemas.account import AccessData, AccountInfo
-
-
-def get_active_user(access_data: AccessData) -> AccountInfo:
-    raise NotImplementedError
--- app/schemas/opponent_search.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class FoundRoomInfo(BaseModel):
-    room_id: int
