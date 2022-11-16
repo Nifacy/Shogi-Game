f499c4cb80bb37a5ce2d64c1e37cf3751af37dac
Nifacy <mai.grishin.alexey@gmail.com>
2022-11-16 15:25:55 +0300

    Revert "Changed ORM models"
    This reverts commit 8fbef1f841a9d70ff8a5b556f5a4d0e8a80ffd87.

--- app/models.py
+++ app/models.py
@@ -7,16 +7,9 @@ class User(Model):
     username = fields.CharField(50, unique=True)
     password_hash = fields.CharField(128)
     rating = fields.IntField(default=0)
-    connected_room = fields.ForeignKeyField(model_name="app.models.Room", related_name="connected_players", null=True)
 
     def verify_password(self, password):
         return bcrypt.verify(password, self.password_hash)
 
     class PydanticMeta:
-        exclude = ('password_hash', 'connected_room')
-
-
-class Room(Model):
-    id = fields.IntField(pk=True)
-    name = fields.CharField(1, default='+')
-    connected_players: fields.ReverseRelation["User"]
+        exclude = ('password_hash',)
