4eedde5d5743c329c9ce3d5b6c9e038fab54cc27
Nifacy <mai.grishin.alexey@gmail.com>
2022-11-23 16:56:47 +0300

    FIxed game model module
    - Game state class now is dataclass (was NamedTuple)
    - Remove opposite moving of base figure's class (moved into move figure command)

--- game_model/figures/base.py
+++ game_model/figures/base.py
@@ -4,16 +4,9 @@ from typing import Iterable
 from game_model.game.model import Figure, Position, Player
 
 
-def _rotate_position(position: Position) -> Position:
-    return Position(x=-position.x, y=-position.y)
-
-
 class BaseFigure(Figure, ABC):
-    _opposite_moving: bool
-
-    def __init__(self, owner: Player, opposite_moving: bool = False):
+    def __init__(self, owner: Player):
         super().__init__(owner=owner)
-        self._opposite_moving = opposite_moving
 
     @abstractmethod
     def get_enable_positions(self) -> Iterable[Position]:
@@ -21,8 +14,4 @@ class BaseFigure(Figure, ABC):
 
     def can_move(self, direction: Position) -> bool:
         enable_positions = self.get_enable_positions()
-
-        if self._opposite_moving:
-            enable_positions = map(_rotate_position, enable_positions)
-
         return direction in enable_positions
